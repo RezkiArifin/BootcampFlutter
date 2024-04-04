5e8c59569794eefc4104226f0594ee73c213aea4
Rezki Fauzan A <rezkhyfauzan01@gmail.com>
2024-04-05 06:36:08 +0700

    Tugas 9

---
+++ SanberAppFlutter/sanber_app/lib/tugas/tugas11/drawerScreen.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class DrawerScreen extends StatefulWidget {
+  const DrawerScreen({super.key});
+
+  @override
+  State<DrawerScreen> createState() => _DrawerScreenState();
+}
+
+class _DrawerScreenState extends State<DrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            accountName: Text("Rezki Fauzan A."),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage("assets/img/passphoto.jpg"),
+            ),
+            accountEmail: Text("rezkhyfauzan01@gmail.com"),
+          ),
+          DrawerListTile(
+            iconData: Icons.group,
+            title: "New Group",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.lock,
+            title: "New Secret Group",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.notifications,
+            title: "New Channel Chat",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.contacts,
+            title: "Contacts",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.bookmark_border,
+            title: "Saved Message",
+            onTilePressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  final IconData? iconData;
+  final String? title;
+  final VoidCallback? onTilePressed;
+
+  const DrawerListTile({
+    Key? key,
+    this.iconData,
+    this.title,
+    this.onTilePressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTilePressed,
+      dense: true,
+      leading: Icon(iconData),
+      title: Text(
+        title!,
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+}
